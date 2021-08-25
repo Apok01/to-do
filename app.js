01106b32a4f6b14290f22f472732c7f8774548bf
Apo <Apok01@users.noreply.github.com>
2021-08-25 21:46:34 +0100

    Broken delte

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 console.log(date);
 
 const app = express();
-let items =["uno","dos","tres"];
+//let items =["uno","dos","tres"];
 let workItems = [];
 
 
@@ -20,44 +20,74 @@ mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser: true, 
 
 const itemSchema = new mongoose.Schema ({
     dataItem:{
-        type: String,
-        required: [true, "Necesary an String"]
+        type: String
+        //required: [true, "Necesary an String"]
     } 
 });
 const Item = mongoose.model("Item", itemSchema);
-const item1 = new Item({
-     dataItem: "1"
-});
-const item2 = new Item({
-    dataItem: "2"
-});
-const item3 = new Item({
-    dataItem : "3"
-});
 
+
+// const item1 = new Item({
+//      dataItem: "1"
+// });
+// const item2 = new Item({
+//     dataItem: "2"
+// });
+// const item3 = new Item({
+//     dataItem : "3"
+// });
+let defaultItems = [];
+
+
+// Item.insertMany(defaultItems, (err)=>{
+//     if (err){
+//         console.log(err);
+//     } else {
+//         console.log("Successfully inserted = " + defaultItems);
+//     }
+// })
 // ------------------------------------------ HOME
 
 
 app.get("/", function(req,res){
-let day = date.getDate();
 
-    res.render("list", {
-        listTitle: day,
-        newListItems: items, 
-    });
+//---------------------------------------------------Find items DB
+Item.find({}, (err, foundItems)=>{
+    if (err){
+        console.log(err);
+    } else {
+        defaultItems = foundItems;
+    //    console.log(defaultItems);
 
-
-});
-app.post("/", (req,res)=>{
-    console.log(req.body);
-    let item = req.body.newItem
-    if (req.body.list === "Work"){
-        workItems.push(item);
-        res.redirect("/Work");
-    }else{
-        items.push(item);
-        res.redirect("/");
     }
+});
+
+let day = date.getDate();
+   res.render("list", {listTitle: day,newListItems: defaultItems});
+   console.log(day);
+});
+
+
+//-------------------------------------------------POST
+app.post("/", (req,res)=>{
+    // console.log(req.body);
+    // let item = req.body.newItem
+    // if (req.body.list === "Work"){
+    //     workItems.push(item);
+    //     res.redirect("/Work");
+    // }else{
+    //     defaultItems.push(item);
+    //     res.redirect("/");
+    // }
+
+    let itemName = req.body.newItem;
+    console.log(itemName);
+    const item1 = new Item ({
+        dataItem: itemName
+    });
+    console.log(item1);
+    item1.save();
+    res.redirect("/");
 });
 
 // ---------------------------------------------- WORK
@@ -79,7 +109,19 @@ app.post("/work",(req,res)=> {
 app.get('/about', (req,res)=>{
     res.render('about');
 });
-
+app.post('/delete', (req, res)=>{
+   if (res.body.checkbox !== (undefined)){
+        let checkedItemId = (req.body.checkbox).replace(/\s/g, '');
+        console.log(checkedItemId + ".");
+        Item.findByIdAndRemove(checkedItemId, (err)=>{
+            if (err){
+                console.log(checkedItemId);
+                console.log(err);
+            } else {
+                console.log("Succesfully Removed id: " + checkedItemId);
+            }
+   });
+}});
 
 
 //---------------------------------------------------- 404
@@ -96,7 +138,7 @@ app.get('*', function(req, res){
 
 
 
-
+//-----------------------------------------------------Server UP
 app.listen(3000, ()=>{
     console.log("Server started on port 3000");
 });
